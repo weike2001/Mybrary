95d5bd273a615f0d1e8cb5417326d5d1e2e98a94
Weike ZHANG <weike@Weikes-MacBook-Air.local>
2023-12-03 04:34:38 -0600

    Finish Book Routes

--- routes/books.js
+++ routes/books.js
@@ -44,26 +44,102 @@ router.post("/", async (req, res) => {
   saveCover(book, req.body.cover);
   try {
     const newBook = await book.save();
-    res.redirect("books");
+    res.redirect(`books/${newBook.id}`);
   } catch {
     renderNewPage(res, book, true);
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id).populate("author").exec();
+    res.render("books/show", { book: book });
+  } catch (err) {
+    console.error(err); // Log the error
+    res.redirect("/");
+  }
+});
+
+// Edit Book Route
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    renderEditPage(res, book);
+  } catch {
+    res.redirect("/");
+  }
+});
+
+// Update Book Route
+router.put("/:id", async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.publishDate = new Date(req.body.publishDate);
+    book.pageCount = req.body.pageCount;
+    book.description = req.body.description;
+    if (req.body.cover != null && req.body.cover !== "") {
+      saveCover(book, req.body.cover);
+    }
+    await book.save();
+    res.redirect(`/books/${book.id}`);
+  } catch (err) {
+    console.log(err);
+    if (book != null) {
+      renderEditPage(res, book, true);
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book, "new", hasError);
+}
+
+// Delete Book Page
+// Delete Book Route
+router.delete("/:id", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    if (!book) {
+      // Book not found or already deleted
+      return res.redirect('/books');
+    }
+    // Redirect to the books list after successful deletion
+    res.redirect("/books");
+  } catch (err) {
+    console.log(err);
+    // Redirect to the homepage if an error occurs
+    res.redirect("/");
+  }
+});
+
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book, "edit", hasError);
+}
+async function renderFormPage(res, book, form, hasError = false) {
   try {
     const authors = await Author.find({});
     const params = {
       authors: authors,
       book: book,
     };
-    if (hasError) params.errorMessage = "Error Creating Book!";
-    res.render("books/new", params);
+    if (hasError) {
+      if (form === "edit") {
+        params.errorMessage = "Error Editing Book!";
+      } else {
+        params.errorMessage = "Error Creating Book!";
+      }
+    }
+    res.render(`books/${form}`, params);
   } catch {
     res.redirect("/books");
   }
 }
-
 function saveCover(book, coverEncoded) {
   if (coverEncoded == null) return;
   const cover = JSON.parse(coverEncoded);
